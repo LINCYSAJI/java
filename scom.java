27c1fd302c8b3f53a516295a9b4816f5da5c4acb
LINCYSAJI <104355791+LINCYSAJI@users.noreply.github.com>
2022-05-30 03:10:25 -0700

    Add files via upload

---
+++ scom.java
@@ -0,0 +1,19 @@
+public class scom
+{
+	public static void main(String[]args)
+	{
+		String str1="Hello";
+		String str2="hello";
+		String str3=str1.toLowerCase();
+		String str4=str2.toLowerCase();
+		int i=str3.compareTo(str4);
+		if(i==0)
+		{
+			System.out.println("both strings are equal");
+		}
+		else
+		{
+			System.out.println("strings are not equal");
+		}
+	}
+}
